786893207ad11f8492d56d7696fe471218dcb56c
Anil Sharma <akieusltd@gmail.com>
2015-08-18 09:28:30 +0100

    Various elementary sort algos

---
+++ algos/src/main/java/SelectionSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/**
+ * @author aks
+ * @since 17/08/15
+ */
+public class SelectionSort extends Sort {
+    public SelectionSort() {
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{7, 6, 1, 2, 5, 9, 4, 3};
+        new SelectionSort().sortIterative(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public void sortIterative(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            exch(arr, i, min);
+        }
+    }
+}
---
+++ algos/src/main/java/Shuffle.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/**
+ * @author aks
+ * @since 17/08/15
+ */
+public class Shuffle extends Sort {
+    public Shuffle() {
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{7, 6, 1, 2, 5, 9, 4, 3};
+        Sort sort = new Shuffle();
+        sort.sortIterative(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("checkCount=" + sort.checkCount);
+        System.out.println("exchCount=" + sort.exchCount);
+    }
+
+    public void sortIterative(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int r = StdRandom.uniform(i);
+            exch(a, r, i);
+        }
+    }
+}
