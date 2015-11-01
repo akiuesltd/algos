4af203b4dbeae394a959fcf0da6142de87929fbf
Anil Sharma <akieusltd@gmail.com>
2015-11-01 09:42:37 +0000

    StSt Test

--- stst/src/test/java/com/akieus/stst/ReferenceRateCalculatorTest.java
+++ stst/src/test/java/com/akieus/stst/ReferenceRateCalculatorTest.java
@@ -1,6 +1,5 @@
 package com.akieus.stst;
 
-import org.apache.commons.math3.stat.descriptive.rank.Percentile;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -20,7 +19,7 @@ public class ReferenceRateCalculatorTest {
     @Test
     public void stalePriceReturnedIfConfiguredButNoPrices() {
         ReferenceRateCalculatorImpl calculator = newCalculator();
-        calculator.onConfiguration(simpleConfiguration());
+        calculator.onConfiguration(aSimpleConfiguration());
 
         assertThat(calculator.calculate(), is(notNullValue()));
         assertThat(calculator.calculate().isStale(), is(true));
@@ -29,7 +28,7 @@ public class ReferenceRateCalculatorTest {
     @Test
     public void stalePriceReturnedIfASingleStalePrice() {
         ReferenceRateCalculatorImpl calculator = newCalculator();
-        calculator.onConfiguration(simpleConfiguration());
+        calculator.onConfiguration(aSimpleConfiguration());
         calculator.onFxPrice(new FxPriceImpl(Double.NaN, Double.NaN, true, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
 
         assertThat(calculator.calculate().isStale(), is(true));
@@ -38,18 +37,10 @@ public class ReferenceRateCalculatorTest {
     @Test
     public void validPriceReturnedIfASingleValidPrice() {
         ReferenceRateCalculatorImpl calculator = newCalculator();
-        calculator.onConfiguration(simpleConfiguration());
+        calculator.onConfiguration(aSimpleConfiguration());
         calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, false, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
 
         assertThat(calculator.calculate().isStale(), is(false));
-    }
-
-    @Test
-    public void midPriceReturnedIfASingleValidPrice() {
-        ReferenceRateCalculatorImpl calculator = newCalculator();
-        calculator.onConfiguration(simpleConfiguration());
-        calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
-
         // validate both bid & offer
         assertThat(calculator.calculate().getBid(), is(closeTo(1.15, 0.01)));
         assertThat(calculator.calculate().getOffer(), is(closeTo(1.15, 0.01)));
@@ -58,7 +49,7 @@ public class ReferenceRateCalculatorTest {
     @Test
     public void stalePriceReturnedIfASingleValidPriceBecomesStale() {
         ReferenceRateCalculatorImpl calculator = newCalculator();
-        calculator.onConfiguration(simpleConfiguration());
+        calculator.onConfiguration(aSimpleConfiguration());
         calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
         assertThat(calculator.calculate().isStale(), is(false));
 
@@ -69,9 +60,23 @@ public class ReferenceRateCalculatorTest {
     }
 
     @Test
+    public void validPriceReturnedIfASingleStalePriceBecomesValid() {
+        ReferenceRateCalculatorImpl calculator = newCalculator();
+        calculator.onConfiguration(aSimpleConfiguration());
+        // valid price
+        calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
+        // make it stale & check
+        calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, true, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
+        assertThat(calculator.calculate().isStale(), is(true));
+        // make it valid again & check
+        calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
+        assertThat(calculator.calculate().isStale(), is(false));
+    }
+
+    @Test
     public void medianOfMidPricesReturnedIfThreePrices() {
         ReferenceRateCalculatorImpl calculator = newCalculator();
-        calculator.onConfiguration(simpleConfiguration());
+        calculator.onConfiguration(aSimpleConfiguration());
         calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
         calculator.onFxPrice(new FxPriceImpl(1.3, 1.4, PriceSource.SOURCE1, PriceProvider.PROVIDER2));
         calculator.onFxPrice(new FxPriceImpl(1.5, 1.6, PriceSource.SOURCE2, null));
@@ -79,16 +84,47 @@ public class ReferenceRateCalculatorTest {
         assertThat(calculator.calculate().getBid(), is(closeTo(1.35, 0.01)));
     }
 
+    @Test
+    public void medianCalculatedCorrectlyIfEvenPricePoints() {
+        ReferenceRateCalculatorImpl calculator = newCalculator();
+        calculator.onConfiguration(aSimpleConfiguration());
+        calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
+        calculator.onFxPrice(new FxPriceImpl(1.3, 1.4, PriceSource.SOURCE1, PriceProvider.PROVIDER2));
+
+        assertThat(calculator.calculate().getBid(), is(closeTo(1.25, 0.01)));
+    }
+
+    @Test
+    public void resettingConfigurationResetsPrices() {
+        ReferenceRateCalculatorImpl calculator = newCalculator();
+        calculator.onConfiguration(aSimpleConfiguration());
+        calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
+        calculator.onFxPrice(new FxPriceImpl(1.3, 1.4, PriceSource.SOURCE1, PriceProvider.PROVIDER2));
+        assertThat(calculator.calculate().isStale(), is(false));
+
+        calculator.onConfiguration(anotherSimpleConfiguration());
+        assertThat(calculator.calculate().isStale(), is(true));
+        calculator.onFxPrice(new FxPriceImpl(1.1, 1.2, PriceSource.SOURCE1, PriceProvider.PROVIDER1));
+        assertThat(calculator.calculate().getBid(), is(closeTo(1.15, 0.01)));
+    }
+
     private ReferenceRateCalculatorImpl newCalculator() {
         return new ReferenceRateCalculatorImpl();
     }
 
-    private Configuration simpleConfiguration() {
+    private Configuration aSimpleConfiguration() {
         return new ConfigurationImpl(new Market[]{
                 new Market(PriceSource.SOURCE1, PriceProvider.PROVIDER1),
                 new Market(PriceSource.SOURCE1, PriceProvider.PROVIDER2),
                 new Market(PriceSource.SOURCE2, null),
                 new Market(PriceSource.SOURCE3, null)
+        });
+    }
+
+    private Configuration anotherSimpleConfiguration() {
+        return new ConfigurationImpl(new Market[]{
+                new Market(PriceSource.SOURCE4, PriceProvider.PROVIDER1),
+                new Market(PriceSource.SOURCE5, null)
         });
     }
 }
